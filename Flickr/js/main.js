324c2525b0168250a1bb6ad83128fa7ace262293
Steve Kwak <kwak@adobe.com>
2018-08-24 16:49:12 -0400

    Flickr example updated

--- Flickr/js/main.js
+++ Flickr/js/main.js
@@ -16,15 +16,15 @@ var FLICKR_API_KEY = 'YOUR_API_KEY';
 var MAX_IMAGE_RESULTS = 30;
 
 var lightSpinner = new Spinner({ lines: 30, length: 0, width: 2, radius: 8, corners: 0, color: '#fff', speed: 2, trail: 100, hwaccel: true, className: 'spinner', zIndex: 2e9 }).spin();
-var darkSpinner  = new Spinner({ lines: 30, length: 0, width: 2, radius: 8, corners: 0, color: '#777', speed: 2, trail: 100, hwaccel: true, className: 'spinner', zIndex: 2e9 }).spin();
-var csInterface  = new CSInterface();
+var darkSpinner = new Spinner({ lines: 30, length: 0, width: 2, radius: 8, corners: 0, color: '#777', speed: 2, trail: 100, hwaccel: true, className: 'spinner', zIndex: 2e9 }).spin();
+var csInterface = new CSInterface();
 
-var cleanFileName = function(name) {
+var cleanFileName = function (name) {
   name = name.split(' ').join('-');
   return name.replace(/\W/g, '');
 };
 
-var createTempFolder = function() {
+var createTempFolder = function () {
   var tempFolderName = 'com.adobe.flickr.extension/';
   var tempFolder = '/tmp/' + tempFolderName;
   if (window.navigator.platform.toLowerCase().indexOf('win') > -1) {
@@ -34,11 +34,11 @@ var createTempFolder = function() {
   return tempFolder;
 };
 
-var downloadAndOpenInPhotoshop = function(url, name, thumb) {
+var downloadAndOpenInPhotoshop = function (url, name, thumb) {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', url, true);
   xhr.responseType = 'arraybuffer';
-  xhr.onload = function(e) {
+  xhr.onload = function (e) {
     if (this.status == 200 || this.status == 304) {
       var uInt8Array = new Uint8Array(this.response);
       var i = uInt8Array.length;
@@ -49,22 +49,22 @@ var downloadAndOpenInPhotoshop = function(url, name, thumb) {
       var base64 = window.btoa(data);
 
       var downloadedFile = createTempFolder() + name + '.jpg';
-      
+
       window.cep.fs.writeFile(downloadedFile, base64, cep.encoding.Base64);
       csInterface.evalScript('openDocument("' + downloadedFile + '")');
       $('.container').masonry('remove', thumb);
       $('.container').masonry('reload');
-    }       
+    }
   };
   xhr.send();
 };
 
-var addThumbToContainer = function(photo) {
+var addThumbToContainer = function (photo) {
   var img_prefix = 'http://farm' + photo.farm + '.static.flickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret;
   var thumb_url = img_prefix + '_m.jpg';
   var real_image_url = img_prefix + '_b.jpg';
   var thumb = $('<div class="thumb"><div class="overlay"></div><img src="' + thumb_url + '" ></img></div>').appendTo('.container');
-  thumb.click(function() {
+  thumb.click(function () {
     var overlay = thumb.find('.overlay');
     thumb.addClass('downloading');
     overlay.append($(lightSpinner.el).clone());
@@ -73,13 +73,13 @@ var addThumbToContainer = function(photo) {
   })
 };
 
-var setupMasonry = function() {
+var setupMasonry = function () {
   var gutterWidth = 2;
   $('.container').masonry({
     isAnimated: true,
     itemSelector: '.thumb',
     gutterWidth: gutterWidth,
-    columnWidth: function(containerWidth) {
+    columnWidth: function (containerWidth) {
       var boxes = Math.ceil(containerWidth / 150);
       var totalGutterSpace = (boxes - 1) * gutterWidth;
       var boxWidth = Math.floor((containerWidth - totalGutterSpace) / boxes);
@@ -89,7 +89,7 @@ var setupMasonry = function() {
   });
 }
 
-var searchFlickr = function(query) {
+var searchFlickr = function (query) {
   var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search';
   url += '&api_key=' + FLICKR_API_KEY;
   url += '&text=' + query;
@@ -99,14 +99,14 @@ var searchFlickr = function(query) {
   $('.container').hide();
   $('.container').empty();
   $.getJSON(url, function (response) {
-    $.each(response.photos.photo, function(i, photo) {
+    $.each(response.photos.photo, function (i, photo) {
       if (i >= MAX_IMAGE_RESULTS) {
         return;
       }
       addThumbToContainer(photo);
     });
 
-    $('.container').imagesLoaded(function() {
+    $('.container').imagesLoaded(function () {
       setupMasonry();
       $('.container').fadeIn('slow');
       $('.container').masonry('reload');
@@ -115,8 +115,8 @@ var searchFlickr = function(query) {
   });
 };
 
-var main = function() {
-  $('.search-box').keypress(function(e) {
+var main = function () {
+  $('.search-box').keypress(function (e) {
     if (e.which == 13) { // enter
       searchFlickr($('.search-box').val());
     }
