fdbdf16197daee21fa71fb285abfd7cf0a22bfdb
Bruce Bullis <brucebullis@gmail.com>
2018-10-23 08:58:28 -0700

    Added copyright
    Added copyright

--- PProPanel/jsx/PPRO/Premiere.jsx
+++ PProPanel/jsx/PPRO/Premiere.jsx
@@ -1,3 +1,16 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+* Copyright 2014 Adobe
+* All Rights Reserved.
+*
+* NOTICE: Adobe permits you to use, modify, and distribute this file in
+* accordance with the terms of the Adobe license agreement accompanying
+* it. If you have received this file from a source other than Adobe,
+* then your use, modification, or distribution of it requires the prior
+* written permission of Adobe. 
+**************************************************************************/
 #include "PPro_API_Constants.jsx"
 
 $._PPP_={
